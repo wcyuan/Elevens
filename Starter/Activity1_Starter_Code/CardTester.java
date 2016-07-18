eca1db2c89045d2e64ed4fe78257286335e0607b
Conan Yuan <conanyuan@gmail.com>
2016-07-18 08:58:40 -0400

    Finish CardTester for activity1

--- Starter/Activity1_Starter_Code/CardTester.java
+++ Starter/Activity1_Starter_Code/CardTester.java
@@ -14,5 +14,24 @@ public class CardTester {
 
 		Card six_of_hearts = new Card("six", "hearts", 6);
 		System.out.println("6H rank = " + six_of_hearts.rank());
+		System.out.println("6H suit = " + six_of_hearts.suit());
+		System.out.println("6H points = " + six_of_hearts.pointValue());
+		System.out.println("6H toString = " + six_of_hearts);
+
+		Card jack_of_spades = new Card("jack", "spades", 11);
+		System.out.println("JS rank = " + jack_of_spades.rank());
+		System.out.println("JS suit = " + jack_of_spades.suit());
+		System.out.println("JS points = " + jack_of_spades.pointValue());
+		System.out.println("JS toString = " + jack_of_spades);
+
+		Card ten_of_diamonds = new Card("ten", "diamnds", 10);
+		System.out.println("10D rank = " + ten_of_diamonds.rank());
+		System.out.println("10D suit = " + ten_of_diamonds.suit());
+		System.out.println("10D points = " + ten_of_diamonds.pointValue());
+		System.out.println("10D toString = " + ten_of_diamonds);
+
+		Card six_of_hearts2 = new Card("six", "hearts", 6);
+		System.out.println("six_of_hearts matches ten_of_diamonds: " + (six_of_hearts.matches(ten_of_diamonds)));
+		System.out.println("six_of_hearts matches six_of_hearts2: " + (six_of_hearts.matches(six_of_hearts2)));
 	}
 }
