e4574934494eaaad9a8b5343078208430009f501
Conan Yuan <conanyuan@gmail.com>
2016-07-19 00:28:04 -0400

    Finish Activity3

--- Starter/Activity3_Starter_Code/Shuffler.java
+++ Starter/Activity3_Starter_Code/Shuffler.java
@@ -39,9 +39,14 @@ public class Shuffler {
 			for (int k = 0; k < values2.length; k++) {
 				System.out.print(" " + values2[k]);
 			}
+			System.out.print(" " + arePermutations(values1, values2));
 			System.out.println();
 		}
 		System.out.println();
+
+		for (int ii = 0; ii < 4; ii++) {
+			System.out.println(flip());
+		}
 	}
 
 
@@ -53,6 +58,20 @@ public class Shuffler {
 	 */
 	public static void perfectShuffle(int[] values) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 3 *** */
+		int[] shuffled = new int[values.length];
+		int k = 0;
+		for (int j = 0; j < (values.length + 1) / 2; j++) {
+			shuffled[k] = values[j];
+			k += 2;
+		}
+		k = 1;
+		for (int j = (values.length + 1) / 2; j < values.length; j++) {
+			shuffled[k] = values[j];
+			k += 2;
+		}
+		for (int j = 0; j < values.length; j++) {
+			values[j] = shuffled[j];
+		}
 	}
 
 	/**
@@ -68,5 +87,63 @@ public class Shuffler {
 	 */
 	public static void selectionShuffle(int[] values) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 3 *** */
+		for (int k = values.length-1; k >= 1; k--) {
+			int r = (int)(Math.random() * values.length);
+			int temp = values[k];
+			values[k] = values[r];
+			values[r] = temp;
+		}
 	}
+
+	/**
+	 * Write a static method named flip that simulates a flip of a weighted coin by returning either
+	 * "heads" or "tails" each time it is called. The coin is twice as likely to turn up heads as tails.
+	 * Thus, flip should return "heads" about twice as often as it returns "tails."
+	 * @return
+	 */
+	public static String flip() {
+		int r = (int)(Math.random() * 3);
+		if (r == 0) {
+			return "tails";
+		} else {
+			return "heads";
+		}
+	}
+
+
+	/*
+	 * Write a static method named arePermutations that, given two int arrays of the same length
+	 * but with no duplicate elements, returns true if one array is a permutation of the other (i.e., the
+	 * arrays differ only in how their contents are arranged). Otherwise, it should return false.
+	 * 
+	 * Here's a simple N^2 implementation that relies on there being no duplicates.  It'd be better to just
+	 * sort both arrays and then check if the result is equal.
+	 */
+	public static boolean arePermutations(int[] first, int[] second) {
+		if (first.length != second.length) {
+			return false;
+		}
+		for (int ii = 0; ii < first.length; ii++) {
+			boolean found = false;
+			for (int jj = 0; jj < second.length; jj++) {
+				if (first[ii] == second[jj]) {
+					found = true;
+					break;
+				}
+			}
+			if (!found) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	/**
+	 * 3. Suppose that the initial contents of the values array in Shuffler.java are {1, 2, 3,
+	 * 4}. For what sequence of random integers would the efficient selection shuffle change values to
+     * contain {4, 3, 2, 1}?
+     * 
+     * 0, 1, 1, 0
+     * 
+	 */
 }
